5fd4023135aae5762a2d7d4f1a855344bd08793c
Hassan Tahseen <ht3aa2001@gmail.com>
2022-04-08 11:11:53 +0300

    4

--- pages/lecture.js
+++ pages/lecture.js
@@ -12,7 +12,7 @@ export default function Lecture() {
       </button>
       <iframe width="350" height="315" src="https://www.youtube.com/embed/m4-HM_sCvtQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       <object data={file} type="application/pdf" width="100%" height="600px">
-        <h5>Your web browser doesn't have a PDF plugin.
+        <h5>Your web browser doesn&apos;t have a PDF plugin.
         Instead you can <a href={file} download>click here to
         download the PDF file.</a></h5>
       </object>
